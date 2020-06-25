9ecf8d153d7db99343d8c59d36d59552c5e1577f
Cem Gencer <cem.gencer@me.com>
2020-06-25 15:02:25 +0300

    dir changed

--- vagrant/puppet/modules/mariadb/manifests/init.pp
+++ vagrant/puppet/modules/mariadb/manifests/init.pp
@@ -1,91 +1,28 @@
-# == Class: mariadbrepo
-#
-# A puppet module that configure the MariaDB repository on a Linux system.
-#
-# === Parameters
-#
-# [*ensure*]
-#   (string) Should the repository file be present on the system
-#   Default: 'present'
-#
-# [*mirror*]
-#   (string) MariaDB mirror (apt only)
-#   Default: 'http://ftp.osuosl.org/pub/mariadb'
-#
-# [*version*]
-#   (float) MariaDB version number
-#   Possible value: 5.5, 10.0
-#   Default: 10.0
-#
-# === Examples
-#
-#  include mariadbrepo
-#
-#    or
-#
-#  class { mariadbrepo:
-#   version => '5.5',
-#  }
-#
-# === Authors
-#
-# Yanis Guenane <yguenane@gmail.com>
-# Dimitri Savineau <savineau.dimitri@gmail.com>
-#
-# === Copyright
-#
-# Copyright 2014 Yanis Guenane
-#
-class mariadbrepo (
-  $ensure  = 'present',
-  $mirror  = 'http://ftp.osuosl.org/pub/mariadb',
-  $version = '10.0',
-) {
+class mariadb () {
 
-  $os = $::operatingsystem ? {
-    'RedHat' => 'rhel',
-    'CentOS' => 'centos',
-    'Fedora' => 'fedora',
-    'Debian' => 'debian',
-    'Ubuntu' => 'ubuntu',
+  exec { "aptGetRepos":
+    command => "sudo curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash",
+    path => ["/bin", "/usr/bin"],
   }
 
-  $os_ver = $::operatingsystemrelease ? {
-    /7.[0-9]+/ => '7',
-    /6.[0-9]+/ => '6',
-    /5.[0-9]+/ => '5',
-    default    => $::operatingsystemrelease,
+  exec { "aptGetMaria":
+    command => "sudo apt install -y --assume-yes --force-yes mariadb-server galera-4 mariadb-client libmariadb3 mariadb-backup mariadb-common",
+    path => ["/bin", "/usr/bin"],
   }
 
-  $arch = $::architecture ? {
-    'i386'   => 'x86',
-    'x86_64' => 'amd64',
-    default  => $::architecture,
+  file { '/tmp/mysql_secure_installation.sql':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/mysql/mysql_secure_installation.sql',
+    require => Exec['aptGetMaria'],
   }
 
-  case $::operatingsystem {
-    'RedHat','CentOS','Fedora': {
-      yumrepo { 'MariaDB' :
-        ensure   => $ensure,
-        descr    => 'MariaDB',
-        baseurl  => "http://yum.mariadb.org/${version}/${os}${os_ver}-${arch}",
-        gpgkey   => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
-        gpgcheck => 1,
-      }
-    }
-    'Debian','Ubuntu': {
-      apt::source { 'MariaDB':
-        ensure     => $ensure,
-        location   => "${mirror}/repo/${version}/${os}",
-        release    => $::lsbdistcodename,
-        repos      => 'main',
-        key        => '1BB943DB',
-        key_server => 'keyserver.ubuntu.com',
-      }
-    }
-    default: {
-      fail ("This module does not support your operating system : ${::operatingsystem}")
-    }
+  exec { 'secure_mariadb':
+    path    => '/usr/bin:/usr/sbin:/bin',
+    command => 'mysql -sfu root < /tmp/mysql_secure_installation.sql',
+    unless  => 'mysql -uroot -ppass',
+    require => File['/tmp/mysql_secure_installation.sql'],
   }
-
 }
